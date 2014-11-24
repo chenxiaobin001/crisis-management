df7344a65d134e4106cf4a9684b05984edce3ca0
eureka1 <lakayiiii@gmail.com>
2014-11-24 12:33:08 -0500

    button text color, group.name display

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,11 +1,11 @@
 class Group < ActiveRecord::Base
-  
+
   has_many :user_groups, :dependent => :destroy
   has_many :users, through: :user_groups
   
   has_many :group_docs, :dependent => :destroy
   has_many :documents, through: :group_docs
-  
+
   validates :group_name, uniqueness: true, presence:true
   
 end
